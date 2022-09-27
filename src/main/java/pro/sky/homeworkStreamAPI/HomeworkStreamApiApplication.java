0f05c713c607129c0a55db42005dd2f0ea6bccc2
Анастасия <grebneva.nastena@yandex.ru>
2022-09-27 14:30:19 +0300

    Initial commit

---
+++ src/main/java/pro/sky/homeworkStreamAPI/HomeworkStreamApiApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkStreamAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkStreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkStreamApiApplication.class, args);
+	}
+
+}
